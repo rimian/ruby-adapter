1c9526b4fa240ff75447470af4f07d5fcc93f450
Rimian Perkins <r.perkins@rimian.com.au>
2012-09-24 21:12:43 +1000

    a new oven!

---
+++ spec/oven_spec.rb
@@ -0,0 +1,7 @@
+require File.dirname(__FILE__) + './../spec/spec_helper'
+require File.dirname(__FILE__) + './../pizza'
+require File.dirname(__FILE__) + './../oven'
+
+describe Oven do
+  it "should bake pizza"
+end
