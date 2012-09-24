2ed5ccffb431149464b4fae37ef3f554bdb576cf
Rimian Perkins <r.perkins@rimian.com.au>
2012-09-24 20:37:35 +1000

    initial pizza

--- spec/categories_spec.rb
+++ spec/categories_spec.rb
@@ -1,3 +1,8 @@
 require File.dirname(__FILE__) + './../spec/spec_helper'
+require File.dirname(__FILE__) + './../pizza'
 
-
+describe Pizza do
+  it "should bake the pizza"
+  it "should be a peperoni pizza"
+  it "should be a mexican pizza"
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,6 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-# require File.expand_path("../../config/environment", __FILE__)
-
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-# Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
+  config.color_enabled = true
   config.order = 'random'
 end
 
